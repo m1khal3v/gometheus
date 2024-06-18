e3cfcd1148b4dc73e7f2980436c1b6363a296841
m1khal3v <m1kh@l3v.moscow>
2024-06-19 00:39:15 +0300

    Incr 6 - Рефакторинг

--- internal/server/mutex/named_mutex.go
+++ internal/server/mutex/named_mutex.go
@@ -24,12 +24,20 @@ func (namedMutex *NamedMutex) getLock(name string) *sync.Mutex {
 	return mutex
 }
 
+func (namedMutex *NamedMutex) TryLock(name string) bool {
+	return namedMutex.getLock(name).TryLock()
+}
+
 func (namedMutex *NamedMutex) Lock(name string) {
 	namedMutex.getLock(name).Lock()
 }
 
 func (namedMutex *NamedMutex) Unlock(name string) {
 	namedMutex.getLock(name).Unlock()
+}
+
+func (namedMutex *NamedMutex) TryLockAll() bool {
+	return namedMutex.mapMutex.TryLock()
 }
 
 func (namedMutex *NamedMutex) LockAll() {
