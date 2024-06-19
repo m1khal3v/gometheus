ab02d2e4223024d20ce1b8b36cdcd0969ed1f6db
m1khal3v <m1kh@l3v.moscow>
2024-06-19 13:50:10 +0300

    Incr 6 - Улучшено логирование

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,6 +11,7 @@ type Config struct {
 	Address        string `env:"ADDRESS"`
 	PollInterval   uint32 `env:"POLL_INTERVAL"`
 	ReportInterval uint32 `env:"REPORT_INTERVAL"`
+	LogLevel       string `env:"LOG_LEVEL"`
 }
 
 func parseConfig() Config {
@@ -18,17 +19,23 @@ func parseConfig() Config {
 	flag.StringVarP(&config.Address, "address", "a", "localhost:8080", "address of gometheus server")
 	flag.Uint32VarP(&config.PollInterval, "poll-interval", "p", 2, "interval of collecting metrics")
 	flag.Uint32VarP(&config.ReportInterval, "report-interval", "r", 10, "interval of reporting metrics")
+	flag.StringVarP(&config.LogLevel, "log-level", "l", "info", "log level")
 	flag.Parse()
 	err := env.Parse(&config)
 	if err != nil {
-		logger.Logger.Fatal(err.Error())
+		panic(err)
 	}
 
 	return config
 }
 
 func main() {
-	defer logger.Logger.Sync()
 	config := parseConfig()
+	err := logger.Init("agent", config.LogLevel)
+	if err != nil {
+		panic(err)
+	}
+
+	defer logger.Logger.Sync()
 	agent.Start(config.Address, config.PollInterval, config.ReportInterval)
 }
---
+++ internal/server/chi/middleware/zap.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+func ZapLogger(logger *zap.Logger, name string) func(next http.Handler) http.Handler {
+	logger = zap.New(logger.Core(), zap.AddCallerSkip(1)).Named(name)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			wrapper := middleware.NewWrapResponseWriter(writer, request.ProtoMajor)
+			timestamp := time.Now()
+			defer func() {
+				logger.Info(
+					"Request processed",
+					zap.String("method", request.Method),
+					zap.String("url", request.URL.String()),
+					zap.Int("status", wrapper.Status()),
+					zap.Int("size", wrapper.BytesWritten()),
+					zap.Duration("duration", time.Since(timestamp)),
+				)
+			}()
+			next.ServeHTTP(wrapper, request)
+		})
+	}
+}
