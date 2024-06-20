1155850e3ce64ab37c59a10fed367a8369cb3753
m1khal3v <m1kh@l3v.moscow>
2024-06-20 10:25:07 +0300

    Incr 7 - Поменял способ создания логгера в middleware

--- internal/server/chi/middleware/zap.go
+++ internal/server/chi/middleware/zap.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ZapLogger(logger *zap.Logger, name string) func(next http.Handler) http.Handler {
-	logger = logger.Named(name)
+	logger = logger.Named(name).WithOptions(zap.AddCallerSkip(2))
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
