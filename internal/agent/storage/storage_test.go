11ac678016d07ebfa1a0bc348bd4fd65fd7b5617
m1khal3v <m1kh@l3v.moscow>
2024-06-22 18:15:59 +0300

    Incr 9 - Добавлен тест на хранилище

---
+++ internal/agent/storage/storage_test.go
@@ -0,0 +1,87 @@
+package storage
+
+import (
+	"github.com/m1khal3v/gometheus/internal/common/metric"
+	"github.com/m1khal3v/gometheus/internal/common/metric/kind/counter"
+	"github.com/m1khal3v/gometheus/internal/common/metric/kind/gauge"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestStorage_Append(t *testing.T) {
+	tests := []struct {
+		name    string
+		metrics []metric.Metric
+	}{
+		{
+			"empty metrics",
+			[]metric.Metric{},
+		},
+		{
+			"append metrics",
+			[]metric.Metric{
+				counter.New("counter", 123),
+				gauge.New("gauge", 123.456),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			storage := New()
+			storage.Append(tt.metrics)
+			for index, metric := range tt.metrics {
+				assert.Equal(t, metric, storage.metrics[index])
+				assert.NotSame(t, metric, storage.metrics[index])
+			}
+		})
+	}
+}
+
+func TestStorage_Remove(t *testing.T) {
+	metrics := []metric.Metric{
+		counter.New("counter", 123),
+		gauge.New("gauge", 123.456),
+	}
+	tests := []struct {
+		name    string
+		filter  filter
+		metrics []metric.Metric
+	}{
+		{
+			name: "remove counters",
+			filter: func(metric metric.Metric) bool {
+				return metric.GetType() == counter.Type
+			},
+			metrics: []metric.Metric{
+				gauge.New("gauge", 123.456),
+			},
+		},
+		{
+			name: "remove value > 123",
+			filter: func(metric metric.Metric) bool {
+				value, _ := strconv.ParseFloat(metric.GetStringValue(), 64)
+				return value > 123
+			},
+			metrics: []metric.Metric{
+				counter.New("counter", 123),
+			},
+		},
+		{
+			name: "remove name contains u",
+			filter: func(metric metric.Metric) bool {
+				return strings.Contains(metric.GetName(), "u")
+			},
+			metrics: []metric.Metric{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			storage := New()
+			storage.Append(metrics)
+			storage.Remove(tt.filter)
+			assert.Equal(t, tt.metrics, storage.metrics)
+		})
+	}
+}
