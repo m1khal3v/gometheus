78f21bbdc891c08c490e5344c368298d73fd29b7
m1khal3v <m1kh@l3v.moscow>
2024-06-25 22:30:37 +0300

    Incr 9 - Добавлен лок на сохранение счетчика

--- internal/server/manager/manager.go
+++ internal/server/manager/manager.go
@@ -6,15 +6,18 @@ import (
 	"github.com/m1khal3v/gometheus/internal/common/metric"
 	"github.com/m1khal3v/gometheus/internal/common/metric/kind/counter"
 	"github.com/m1khal3v/gometheus/internal/common/metric/kind/gauge"
+	"github.com/m1khal3v/gometheus/internal/server/mutex"
 	"github.com/m1khal3v/gometheus/internal/server/storage"
 )
 
 type Manager struct {
+	mutex   *mutex.NamedMutex
 	storage storage.Storage
 }
 
 func New(storage storage.Storage) *Manager {
 	return &Manager{
+		mutex:   mutex.NewNamedMutex(),
 		storage: storage,
 	}
 }
@@ -46,6 +49,9 @@ func (manager *Manager) Save(metric metric.Metric) metric.Metric {
 }
 
 func (manager *Manager) saveCounter(metric *counter.Metric) {
+	manager.mutex.Lock(metric.GetName())
+	defer manager.mutex.Unlock(metric.GetName())
+
 	previous := manager.storage.Get(metric.GetName())
 	if previous != nil && previous.GetType() == metric.GetType() {
 		metric.Add(previous.(*counter.Metric).GetValue())
