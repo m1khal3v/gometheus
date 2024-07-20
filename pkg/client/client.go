a4179b96f4d2bbfb789a16697a6322ace11b52dc
m1khal3v <m1kh@l3v.moscow>
2024-07-20 17:11:03 +0300

    Incr 14 - фикс

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -47,12 +47,14 @@ func NewSignatureConfig(header, key string, hash func() hash.Hash) *signatureCon
 
 type Config struct {
 	Address   string
-	Transport http.RoundTripper
 	Signature *signatureConfig
 
 	DisableCompress          bool
 	DisableAddressValidation bool
 	DisableRetry             bool
+
+	// for tests
+	transport http.RoundTripper
 }
 
 type Client struct {
@@ -62,6 +64,50 @@ type Client struct {
 }
 
 type preRequestHook func(config *Config, request *http.Request) error
+
+type BufferReader struct {
+	*bytes.Reader
+}
+
+func (buffer *BufferReader) Close() error {
+	return nil
+}
+
+func (buffer *BufferReader) ReadAll() ([]byte, error) {
+	if _, err := buffer.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
+
+	return io.ReadAll(buffer)
+}
+
+type transportHook func(*http.Response) (*http.Response, error)
+
+func (function transportHook) RoundTrip(req *http.Request) (*http.Response, error) {
+	response, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return function(response)
+}
+
+var transport transportHook = func(response *http.Response) (*http.Response, error) {
+	if response.Body == nil {
+		return response, nil
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+
+	_, err := io.Copy(buffer, response.Body)
+	if err = errors.Join(err, response.Body.Close()); err != nil {
+		return nil, err
+	}
+
+	response.Body = &BufferReader{Reader: bytes.NewReader(buffer.Bytes())}
+
+	return response, nil
+}
 
 type ErrUnexpectedStatus struct {
 	Status int
@@ -100,7 +146,7 @@ func New(config *Config) (*Client, error) {
 
 	client := resty.
 		New().
-		SetTransport(config.Transport).
+		SetTransport(config.transport).
 		SetBaseURL(config.Address).
 		SetHeader("Accept-Encoding", "gzip")
 
@@ -128,8 +174,12 @@ func prepareConfig(config *Config) error {
 		}
 	}
 
-	if config.Transport == nil {
-		config.Transport = http.DefaultTransport
+	if config.transport == nil {
+		if config.Signature != nil {
+			config.transport = transport
+		} else {
+			config.transport = http.DefaultTransport
+		}
 	}
 
 	return nil
@@ -288,11 +338,22 @@ func (client *Client) doRequest(request *resty.Request, method, url string) (*re
 		})
 	}
 
-	if err == nil && client.signature != nil && !client.signature.DisableResponseValidation {
-		// TODO: тут надо заюзать NoDecodeResponse и разгзипливать самостоятельно
-		expectedSignature := hmac.
-			New(client.signature.hash, []byte(client.signature.key)).
-			Sum(result.Body())
+	if err != nil {
+		return result, err
+	}
+
+	if client.signature != nil && !client.signature.DisableResponseValidation {
+		encoder := hmac.New(client.signature.hash, []byte(client.signature.key))
+		body, err := result.RawResponse.Body.(*BufferReader).ReadAll()
+		if err != nil {
+			return nil, err
+		}
+
+		if _, err := encoder.Write(body); err != nil {
+			return nil, err
+		}
+
+		expectedSignature := encoder.Sum(nil)
 		resultSignature, err := hex.DecodeString(result.Header().Get(client.signature.header))
 		if err != nil {
 			return nil, err
